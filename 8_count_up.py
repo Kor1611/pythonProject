d718f199be534174279873610b7856e65ef74a3f
Kor1611 <kjv1611@ukr.net>
2021-11-21 23:43:25 +0200

    111

---
+++ 8_count_up.py
@@ -0,0 +1,10 @@
+def count_digits(x):  # Сумма цифр числа
+    sum = 0;
+    while x > 0:
+        sum += x % 10
+        x = round(x / 10)
+    return sum
+
+
+y = int(input('Input number:'))
+print('Sum digits of number = ', count_digits(y))
